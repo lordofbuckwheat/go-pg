a26521f3f5fe81af8fafbb57708f78f9b612ea1a
lordofbuckwheat <lord.of.buckwheat@gmail.com>
2019-12-19 08:02:00 +0300

    .

---
+++ gorm/go.mod
@@ -0,0 +1,9 @@
+module github.com/lordofbuckwheat/go-pg
+
+go 1.13
+
+require (
+	github.com/gorilla/websocket v1.4.1
+	github.com/jinzhu/gorm v1.9.11
+	github.com/lucasb-eyer/go-colorful v1.0.3
+)
---
+++ gorm/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	db, err := gorm.Open("mysql", "root:root@tcp(localhost:3306)/tvbit_test?charset=utf8mb4")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer func() {_ = db.Close()}()
+}
