f825ae8d23bdf14e9fec6647ccc3ea89467a165d
lordofbuckwheat <lord.of.buckwheat@gmail.com>
2019-12-22 13:34:09 +0300

    .

--- gorm/main.go
+++ gorm/main.go
@@ -17,12 +17,20 @@ type User struct {
 	Password  string `gorm:"NOT NULL"`
 	AccountID uint64
 	Account   *Account
+	Profile   *Profile
+}
+
+type Profile struct {
+	Model
+	Name   string `gorm:"NOT NULL"`
+	UserID uint64
+	User   *User
 }
 
 type Account struct {
 	Model
 	Title string `gorm:"NOT NULL"`
-	Users []User
+	Users []*User
 }
 
 func main() {
@@ -35,6 +43,7 @@ func main() {
 	db.AutoMigrate(
 		&Account{},
 		&User{},
+		&Profile{},
 	)
 	var account = &Account{
 		Title: "account 1",
@@ -42,16 +51,37 @@ func main() {
 	if err := db.Create(account).Error; err != nil {
 		panic(err)
 	}
-	if err := db.Model(account).Association("Users").Replace([]User{{
+	var users = []*User{{
 		Login:    "user1",
 		Password: "password1",
+		Profile: &Profile{
+			Name: "profile 1",
+		},
+		Account: account,
 	}, {
 		Login:    "user2",
 		Password: "password2",
-	}}).Error; err != nil {
+	}}
+	if err := db.Model(account).Association("Users").Replace(users).Error; err != nil {
+		panic(err)
+	}
+	fmt.Println("account", MustMarshal(account), MustMarshal(users))
+	users[0].Profile = &Profile{
+		Name: "profile 2",
+	}
+	if err := db.Save(users[0]).Error; err != nil {
+		panic(err)
+	}
+	fmt.Println("user after replace", MustMarshal(users[0]))
+	account.Users = []*User{{
+		Login:    "user3",
+		Password: "password3",
+	}}
+	if err := db.Save(account).Error; err != nil {
 		panic(err)
 	}
 	fmt.Println("account", MustMarshal(account))
+	account.Users = nil
 	var user = &User{}
 	if err := db.Find(user).Error; err != nil {
 		panic(err)
@@ -61,7 +91,13 @@ func main() {
 	if err := db.Model(user).Association("Account").Find(account2).Error; err != nil {
 		panic(err)
 	}
+	user.Account = account2
 	fmt.Println("user", MustMarshal(user), MustMarshal(account2))
+	users = nil
+	if err := db.Model(account2).Association("Users").Find(&users).Error; err != nil {
+		panic(err)
+	}
+	fmt.Println("account2", MustMarshal(account2), MustMarshal(users))
 }
 
 func MustMarshal(src interface{}) string {
