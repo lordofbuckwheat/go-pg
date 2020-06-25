a71013e0b714fe393a65a7e0f798727dabae836a
lordofbuckwheat <lord.of.buckwheat@gmail.com>
2020-06-25 09:11:51 +0000

    .

--- sendmail/main.go
+++ sendmail/main.go
@@ -1,26 +1,94 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
+	"net/mail"
 	"net/smtp"
 )
 
 const (
-	host     = "smtp.gmail.com"
-	port     = 587
-	from     = "lord.of.buckwheat@gmail.com"
+	host     = "smtp.yandex.ru"
+	port     = 465
+	from     = "noreply@tvbit.co"
 	to       = "goo007@mail.ru"
-	password = ""
-	subject  = "test subject 2"
-	body     = "test body 2"
+	password = "xSVMr7WHpszf7DWs"
+	subject  = "test subject 4"
+	body     = "test body 4"
 )
 
 func main() {
+	from1 := mail.Address{Address: from}
+	to := mail.Address{Address: to}
+
+	// Setup headers
+	headers := make(map[string]string)
+	headers["From"] = from1.String()
+	headers["To"] = to.String()
+	headers["Subject"] = subject
+
+	// Setup message
+	message := ""
+	for k, v := range headers {
+		message += fmt.Sprintf("%s: %s\r\n", k, v)
+	}
+	message += "\r\n" + body
+
+	// Connect to the SMTP Server
+	servername := fmt.Sprintf("%s:%d", host, port)
+
 	auth := smtp.PlainAuth("", from, password, host)
-	msg := fmt.Sprintf("To: %s\r\nSubject: %s\r\n\r\n%s\r\n", to, subject, body)
-	err := smtp.SendMail(fmt.Sprintf("%s:%d", host, port), auth, from, []string{to}, []byte(msg))
+
+	// TLS config
+	tlsconfig := &tls.Config{
+		InsecureSkipVerify: true,
+		ServerName:         host,
+	}
+
+	// Here is the key, you need to call tls.Dial instead of smtp.Dial
+	// for smtp servers running on 465 that require an ssl connection
+	// from the very beginning (no starttls)
+	conn, err := tls.Dial("tcp", servername, tlsconfig)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("done")
+
+	c, err := smtp.NewClient(conn, host)
+	if err != nil {
+		panic(err)
+	}
+
+	// Auth
+	if err = c.Auth(auth); err != nil {
+		panic(err)
+	}
+
+	// To && From
+	if err = c.Mail(from1.Address); err != nil {
+		panic(err)
+	}
+
+	if err = c.Rcpt(to.Address); err != nil {
+		panic(err)
+	}
+
+	// Data
+	w, err := c.Data()
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = w.Write([]byte(message))
+	if err != nil {
+		panic(err)
+	}
+
+	err = w.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	if err := c.Quit(); err != nil {
+		panic(err)
+	}
 }
